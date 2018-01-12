cdc5d21defdfb14c0fb091b4e84a152bbb1b6005
taoman1984 <35364312+taoman1984@users.noreply.github.com>
2018-01-12 08:40:14 +0000

    Create hello.c
    Say hi to github.

---
+++ hello.c
@@ -0,0 +1,15 @@
+/*******************************************************
+File:   hello.c
+Date:   2018-1-12
+Author: Taoman
+    First project on github. Have fan.
+*******************************************************/
+
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    printf("Hello, cruel world!\n");
+    
+    return 1;
+}
